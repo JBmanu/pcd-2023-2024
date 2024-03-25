42ab185fc88d50e3512b447998b00702a5d66c63
Manuel Buizo <65312924+JBmanu@users.noreply.github.com>
2024-03-25 14:02:03 +0100

    feat: FakeBarrier

--- app/src/main/java/pcd/lab05/monitors/ex_barrier/FakeBarrier.java
+++ app/src/main/java/pcd/lab05/monitors/ex_barrier/FakeBarrier.java
@@ -1,15 +1,32 @@
 package pcd.lab05.monitors.ex_barrier;
 
+import java.util.concurrent.locks.*;
+
 /*
  * Barrier - to be implemented
  */
 public class FakeBarrier implements Barrier {
-	
-	public FakeBarrier(int nParticipants) {	}
-	
-	@Override
-	public void hitAndWaitAll() throws InterruptedException {	
-	}
+	private final ReentrantLock mutex;
+	private final Condition condition;
+    final int participants;
+    int counter;
 
-	
+    public FakeBarrier(final int nParticipants) {
+        this.mutex = new ReentrantLock();
+        this.condition = this.mutex.newCondition();
+        this.participants = nParticipants;
+        this.counter = 0;
+
+    }
+
+    @Override
+    public void hitAndWaitAll() throws InterruptedException {
+		this.mutex.lock();
+		this.counter += 1;
+        while (this.counter < this.participants) this.condition.await();
+		this.condition.signalAll();
+		this.mutex.unlock();
+    }
+
+
 }
